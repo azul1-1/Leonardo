d06db765446e2361c9e2a58bbf4ebb25cc72ab65
azul1-1 <naguanagua.leo@gmail.com>
2024-01-24 15:11:09 -0400

    Update TwoFactor.php

--- app/Http/Middleware/TwoFactor.php
+++ app/Http/Middleware/TwoFactor.php
@@ -3,36 +3,37 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class TwoFactor
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
-
-        $user=auth()->user();
+        $user = auth()->user();
 
         if(auth()->check() && $user->two_factor_code)
-        {if($user->two_factor_expires_at->lt(now()))
-            
-            
+        {
+            if($user->two_factor_expires_at<now()) //expired
             {
                 $user->resetTwoFactorCode();
                 auth()->logout();
-                return redirect()->route('login')->withMessage('The two factor code has expired');
+
+                return redirect()->route('login')
+                ->withMessage('The two factor code has expired. Please login again.');
             }
 
-            if(!$request->is('verify*')){
+            if(!$request->is('verify*')) //prevent enless loop, otherwise send to verify
+            {
                 return redirect()->route('verify.index');
             }
-
         }
+
         return $next($request);
     }
 }
