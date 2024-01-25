94e4d68519ce1ed52dd1780fba32f747a1d81a32
azul1-1 <naguanagua.leo@gmail.com>
2024-01-25 16:16:38 -0400

    Update twoFactor.blade.php

--- resources/views/auth/twoFactor.blade.php
+++ resources/views/auth/twoFactor.blade.php
@@ -1,56 +1,112 @@
 
 
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card-group">
-            <div class="card p-4">
-                <div class="card-body">
-                    @if(session()->has('message'))
-                        <p class="alert alert-info">
-                            {{ session()->get('message') }}
-                        </p>
-                    @endif
-                    <form method="POST" action="{{ route('verify.store') }}">
-                        {{ csrf_field() }}
-                        <h1>Two Factor Verification</h1>
-                        <p class="text-muted">
-                            You have received an email which contains two factor login code.
-                            If you haven't received it, press <a href="{{ route('verify.resend') }}">here</a>.
-                        </p>
+<!DOCTYPE html>
+<html lang="en">
 
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fa fa-lock"></i>
-                                </span>
-                            </div>
-                            <input name="two_factor_code" type="text" class="form-control{{ $errors->has('two_factor_code') ? ' is-invalid' : '' }}" required autofocus placeholder="Two Factor Code">
-                            @if($errors->has('two_factor_code'))
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form method="POST" action="{{ route('verify.store')}}" >
+                                    @csrf
+                                        <div class="form-group">
+                                            <input type="text" 
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                 name="two_factor_code"  class="form-control{{ $errors->has('two_factor_code') ? ' is-invalid' : '' }}" required autofocus placeholder="Two Factor Code">
+                                                 @if($errors->has('two_factor_code'))
                                 <div class="invalid-feedback">
                                     {{ $errors->first('two_factor_code') }}
                                 </div>
                             @endif
-                        </div>
-
-                        <div class="row">
-                            <div class="col-6">
-                                <button type="submit" class="btn btn-primary px-4">
-                                    Verify
-                                </button>
-                            </div>
-                            <div class="col-6 text-right">
+                                               
+                                        </div>
+                                       
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Enter</button>
+                                        
+                                        
+                                        <hr>
+                                        
+                                    </form>
+                                    <hr>
+                                    <div class="col-6 text-right">
                                 <a class="btn btn-danger px-4" href="#" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
                                     {{ trans('global.logout') }}
                                 </a>
                             </div>
+                                    
+                                    
+                                </div>
+                            </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
 
-<form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
-    {{ csrf_field() }}
-</form>
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
