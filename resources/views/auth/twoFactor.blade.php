32aa4beaa5e372866eee8f9a0ee24b47cba91b3c
azul1-1 <naguanagua.leo@gmail.com>
2024-01-25 16:32:55 -0400

    Update twoFactor.blade.php

--- resources/views/auth/twoFactor.blade.php
+++ resources/views/auth/twoFactor.blade.php
@@ -63,6 +63,13 @@
                                         <hr>
                                         
                                     </form>
+                                    <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
 
 
                                     <form method="post" action="{{url('logout')}}">
