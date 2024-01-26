c092cf1d322bfb4b871cbf5e8f818910f162f3a9
azul1-1 <naguanagua.leo@gmail.com>
2024-01-26 11:52:54 -0400

    asas

--- app/Http/Controllers/Auth/TwoFactorController.php
+++ app/Http/Controllers/Auth/TwoFactorController.php
@@ -47,14 +47,7 @@ class TwoFactorController extends Controller
         return redirect()->back()->withMessage('The two factor code has been sent again');
     }
 
-    public function toggleSecondAuth(Request $request)
-{
-    $user = User::find($request->user_id);
-    $user->verify_confirmation = $request->verify_confirmation;
-    $user->save();
-
-    return response()->json(['success'=>'Status change successfully.']);
-}
+    
 
 
 }
---
+++ app/Http/Controllers/VerifyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class VerifyController extends Controller
+{
+    public function toggleSecondAuth(Request $request)
+{
+    $user = User::find($request->user_id);
+    $user->verify_confirmation = $request->verify_confirmation;
+    $user->save();
+
+    return response()->json(['success'=>'Status change successfully.']);
+}
+}
