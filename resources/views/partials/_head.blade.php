b73f69172bac5ecb9a1bd416ff0f626246cc7beb
azul1-1 <naguanagua.leo@gmail.com>
2024-01-26 11:43:07 -0400

    Update _head.blade.php

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -21,4 +21,10 @@
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
+    <script>   $.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+  }
+});</script>
+
 </head>
